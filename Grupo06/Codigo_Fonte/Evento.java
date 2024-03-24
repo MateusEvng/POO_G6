5b62e4558fa2b4fc19dec523d0048cd677fc8cf8
Mateus Evangelista <105022974+MateusEvng@users.noreply.github.com>
2024-03-24 12:58:41 -0300

    Upload | Evento_v2 e Usuario_v2

--- Grupo06/Codigo_Fonte/Evento.java
+++ Grupo06/Codigo_Fonte/Evento.java
@@ -4,29 +4,50 @@ import java.time.temporal.ChronoUnit;
 
 public class Evento{
     private Data DataInicio,DataFim;
-    private String local,nome;
-    private Usuario[] convidados = new Usuario[50];
+    private String local,titulo;
+    private Usuario[] convidados = new Usuario[50]; //mudar pra arraylist na v3
     private int contadorConvidados;
     private boolean confirmado;
-    public Evento(String nome, String local, Data DataInicio, Data DataFim, Usuario[] convidados, int contadorConvidados, boolean confirmado){
-        this.nome = nome;
+
+    //construtores
+    
+    public Evento(String titulo, String local, int diaInicio, int mesInicio, int anoInicio, int diaFim, int mesFim, int anoFim, int horaIncio, int minutoInicio, int horaFim, int minutoFim){
+
+        this.titulo = titulo;
         this.local = local;
+
+        DataInicio = new Data(diaInicio, mesInicio, anoInicio, horaIncio, minutoInicio);
+        DataFim = new Data(diaFim, mesFim, anoFim, horaFim, minutoFim);
+
+        //continuar na v3
+        
+
+    }
+
+    /*public Evento(String titulo, String local, Data DataInicio, Data DataFim){
+
+        this.titulo = titulo;
+        this.local = local;
+        /*
         if(validaDatas(DataInicio, DataFim)){
             this.DataInicio = DataInicio;
             this.DataFim = DataFim;
         }
         else throw new IllegalArgumentException("Datas de Inicio e Fim inválidas.");
-        this.confirmado = confirmado;
+        //this.confirmado = confirmado;
+        /*
         this.contadorConvidados = contadorConvidados;
         for(int i=0;i<contadorConvidados;i++){
             this.convidados[i] = convidados[i];
         }
-    }
-    public void verListaConvidados(){
+    }*/  
+
+    //metodos
+    /*public void verListaConvidados(){
         for(int i=0;i<contadorConvidados;i++){
-            System.out.println("Convidado "+(i+1)+": "+convidados[i].getNome());
+            System.out.println("Convidado "+(i+1)+": "+convidados[i].getitulo());
         }
-    }
+    }*/
     public void DuracaoEvento(){
         LocalDateTime inicio = LocalDateTime.of(DataInicio.getAno(), DataInicio.getMes(), DataInicio.getDia(), DataInicio.getHora(), DataInicio.getMinuto());
         LocalDateTime fim = LocalDateTime.of(DataFim.getAno(), DataFim.getMes(), DataFim.getDia(), DataFim.getHora(), DataFim.getMinuto());
@@ -35,38 +56,118 @@ public class Evento{
         long diferencaEmMinutos = ChronoUnit.MINUTES.between(inicio, fim)%60;
         System.out.printf("O Evento tem duração de %d dia(s), %d hora(s), %d minuto(s)\n", diferencaEmDias, diferencaEmHoras, diferencaEmMinutos);
     }
-    public void VisualizarEvento(){
-        System.out.println("Nome do Evento: "+nome);
+    /*
+    public void visualizarEvento(){
+        System.out.println("titulo do Evento: "+titulo);
         System.out.println("Local: "+local);
         System.out.println("Data de Início: "+DataInicio.toString());
         System.out.println("Data de Fim: "+DataFim.toString());
         System.out.println("Confirmado: "+(confirmado ? "Sim" : "Não"));
         System.out.println("Número de Convidados: "+contadorConvidados);
     }
+    */
+    @Override
+    public String toString() {
+        String eventoTitulo = "titulo do Evento: "+ this.getTitulo();
+        String eventoLocal = "Local: "+ this.getLocal();
+        String dtInicio = "Data de Início: "+ DataInicio.toString();
+        String dtFim = "Data de Fim: "+ DataFim.toString();
+        String confirmado = "Confirmado: "+ (this.confirmado ? "Sim" : "Não");
+        String numConvidados = "Número de Convidados: "+ String.valueOf(contadorConvidados);
+
+        String eventoString = eventoTitulo + System.lineSeparator() + eventoLocal + System.lineSeparator() + dtInicio + System.lineSeparator() + dtFim + System.lineSeparator() + confirmado + System.lineSeparator() + numConvidados;
+
+        return eventoString;
+    }
+
     private boolean validaDatas(Data DataInicio, Data DataFim) {
         LocalDateTime inicio = LocalDateTime.of(DataInicio.getAno(), DataInicio.getMes(), DataInicio.getDia(), DataInicio.getHora(), DataInicio.getMinuto());
         LocalDateTime fim = LocalDateTime.of(DataFim.getAno(), DataFim.getMes(), DataFim.getDia(), DataFim.getHora(), DataFim.getMinuto());
         return inicio.isBefore(fim);
     }
-    public String getNome(){
-        return nome;
+
+    //getters e setters
+    public String getTitulo(){
+        return titulo;
     }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
     public String getLocal(){
         return local;
     }
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
     public Data getDataInicio(){
         return DataInicio;
     }
+    public void setDataInicio(int dia, int mes, int ano) {
+        DataInicio.setDia(dia);
+        DataInicio.setMes(mes);
+        DataInicio.setAno(ano);
+    }
+
     public Data getDataFim(){
         return DataFim;
     }
+    public void setDataFim(int dia, int mes, int ano) {
+        DataFim.setDia(dia);
+        DataFim.setMes(mes);
+        DataFim.setAno(ano);
+    }
+
     public Usuario[] getConvidados(){
         return convidados;
     }
+    public void setConvidados(Usuario[] convidados) {
+        this.convidados = convidados;
+    }
+
     public int getContadorConvidados(){
         return contadorConvidados;
     }
+    public void setContadorConvidados(int contadorConvidados) {
+        this.contadorConvidados = contadorConvidados;
+    }
+
     public boolean getConfirmado(){
         return confirmado;
+    }
+    public void setConfirmado(boolean confirmado) {
+        this.confirmado = confirmado;
+    }
+
+    //alteração de data (específicos)
+    public void alteraDiaDataInicio(int dia){
+        DataInicio.setDia(dia);
+    }
+    public void alteraMesDataInicio(int mes){
+        DataInicio.setMes(mes);
+    }
+    public void alteraAnoDataInicio(int ano){
+        DataInicio.setAno(ano);
+    }
+
+    public void alteraDiaDataFim(int dia){
+        DataFim.setDia(dia);
+    }
+    public void alteraMesDataFim(int mes){
+        DataFim.setDia(mes);
+    }
+    public void alteraAnoDataFim(int ano){
+        DataFim.setDia(ano);
+    }
+
+    //alteração de hora
+    public void alteraHoraInicio(int hora, int minuto){
+        DataInicio.setHora(hora);
+        DataInicio.setMinuto(minuto);
+    }
+    public void alteraHoraFim(int hora, int minuto){
+        DataFim.setHora(hora);
+        DataFim.setMinuto(minuto);
     }
 }
--- Grupo06/Codigo_Fonte/Usuario.java
+++ Grupo06/Codigo_Fonte/Usuario.java
@@ -1,34 +1,75 @@
 package Codigo_Fonte;
 
-public class usuario {
     public class Usuario {
+        
         private String nome;
         private String email;
         private String senha;
         private Agenda agenda;
+
+
         //construtores 
         public Usuario(String nome, String email, String senha) {
             this.nome = nome;
             this.email = email;
             this.senha = senha;
-            this.agenda = new Agenda();
+            this.agenda = new Agenda(this);
         }
+
+
         //metodos
+
+        //criação e cancelamento de eventos
+
+/*
         public void criarEvento(Evento evento) {
+            this.agenda.adicionarEvento(evento);
+        }
+ */
+        public void criarEvento(String titulo, String local, int diaInicio, int mesInicio, int anoInicio, int diaFim, int mesFim, int anoFim, int horaIncio, int minutoInicio, int horaFim, int minutoFim){
+            Evento evento = new Evento(titulo, local, diaInicio, mesInicio, anoInicio, diaFim, mesFim, anoFim, horaIncio, minutoInicio, horaFim, minutoFim);
+
             agenda.adicionarEvento(evento);
         }
 
-        public void cancelarEvento(Evento evento) {
-            agenda.removerEvento(evento);
+        public void cancelarEvento(String titulo) {
+            agenda.removerEvento(titulo);
         }
-
+/*
         public void editarEvento(Evento evento, String novoNome, LocalDateTime novaData) {
             evento.setNome(novoNome);
             evento.setData(novaData);
-        }
-
+        } 
+/*
         public void visualizarEvento(Evento evento) {
             System.out.println(evento);
         }
-    }
+*/
+        public void confirmaEvento(Evento evento){
+            evento.setConfirmado(true);
+        }
+
+        public void recusaEvento(Evento evento){
+            evento.setConfirmado(false);
+        }
+
+        //getters e setters
+        public String getEmail() {
+            return email;
+        }
+        public void setEmail(String email) {
+            this.email = email;
+        }
+        
+        public String getNome() {
+            return nome;
+        }
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public void setSenha(String senha) {
+            this.senha = senha;
+        }
 }
+
